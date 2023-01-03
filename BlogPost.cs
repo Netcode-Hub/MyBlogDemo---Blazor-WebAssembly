b8f2264edfa6a37e9a4f9fafe962977094644540
Netcode-Hub <110794348+Netcode-Hub@users.noreply.github.com>
2023-01-03 04:24:56 +0000

    Add files via upload

---
+++ BlogPost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MyBlogDemo.Shared
+{
+    
+    public class BlogPost
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Url is required"), StringLength(100, ErrorMessage = "Maximum length must be around 100")]
+        public string Url { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Title is required"), StringLength(100, ErrorMessage = "Maximum length must be around 100")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; } = string.Empty;
+        public string? Author { get; set; }
+        public string? Image { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsPublished { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ BlogsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBlogDemo.Server.Repositories;
+using MyBlogDemo.Shared;
+
+namespace MyBlogDemo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly IBlogRepository blogRepository;
+
+        public BlogsController(IBlogRepository blogRepository)
+        { this.blogRepository = blogRepository; }
+
+        [HttpGet]
+        public async Task<ActionResult<BlogPost>> GimmeAllPosts()
+        { return Ok(await blogRepository.GetAllBlogPosts()); }
+
+
+        [HttpGet("{url}")]
+        public async Task<ActionResult<BlogPost>> GimmeSinglePost(string url)
+        { return Ok(await blogRepository.GetBlogPostUrl(url)); }
+
+
+        [HttpPost]
+        public async Task<ActionResult<BlogPost>> CreateNewBlogPost(BlogPost request)
+        { return Ok( await blogRepository.CreateNewBlogPost(request)); }
+
+        [HttpGet("search/{searchText}")]
+        public async Task<ActionResult<List<BlogPost>>> SearchBlogs(string searchText)
+        { return Ok(await blogRepository.SearchBlogs(searchText)); }
+    }
+}
